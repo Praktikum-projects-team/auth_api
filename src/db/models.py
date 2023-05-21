84094c67f1061df16295d4890bc51cd133c9daaf
Liana <liana.nigmatullina@tetrika.school>
2023-05-21 19:02:16 +0300

    integrate

--- src/db/models.py
+++ src/db/models.py
@@ -3,10 +3,25 @@ from datetime import datetime
 
 from sqlalchemy import Column, String, ForeignKey, DateTime
 from sqlalchemy.dialects.postgresql import UUID
-from db import Base
+from db.pg_db import db
 
 
-class User(Base):
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(String, default='')
+
+    def __init__(self, name, description=None):
+        self.name = name
+        self.description = description
+
+    def __repr__(self):
+        return f'<Role {self.name}(id: {self.id})>'
+
+
+class User(db.Model):
     __tablename__ = 'users'
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
@@ -14,16 +29,18 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
     name = Column(String, nullable=True)
+    roles = db.relationship(Role, secondary='user_roles')
 
-    def __init__(self, login, password):
+    def __init__(self, login, password, name=None):
         self.login = login
         self.password = password
+        self.name = name
 
     def __repr__(self):
         return f'<User {self.login}>'
 
 
-class LoginHistory(Base):
+class LoginHistory(db.Model):
     __tablename__ = 'login_history'
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
@@ -39,22 +56,7 @@ class LoginHistory(Base):
         return f'<LoginHistory {self.id} for user {self.user_id}>'
 
 
-class Role(Base):
-    __tablename__ = 'roles'
-
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    name = Column(String, nullable=False)
-    description = Column(String, default='')
-
-    def __init__(self, name, description=None):
-        self.name = name
-        self.description = description
-
-    def __repr__(self):
-        return f'<Role {self.name}(id: {self.id})>'
-
-
-class UserRole(Base):
+class UserRole(db.Model):
     __tablename__ = 'user_roles'
 
     user_id = Column(UUID(as_uuid=True), ForeignKey(User.id), primary_key=True, nullable=False)
