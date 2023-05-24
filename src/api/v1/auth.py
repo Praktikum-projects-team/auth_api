f8d8e68e2bb760e92aade34fb85fdb3b69019c93
Liana <liana.nigmatullina@tetrika.school>
2023-05-24 17:40:29 +0300

    auth

--- src/api/v1/auth.py
+++ src/api/v1/auth.py
@@ -1,19 +1,35 @@
 from flask import jsonify
 from flask import request
+from flask import Blueprint
+
 
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 
-from main import app
+auth_bp = Blueprint('auth', __name__)
 
-
-@app.route("/login", methods=["POST"])
-def login():
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
-    if username != "test" or password != "test":
-        return jsonify({"msg": "Bad username or password"}), 401
-
-    access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token)
+#
+# @auth_bp.route('/login', methods=['POST'])
+# def login():
+#     username = request.json.get("username", None)
+#     password = request.json.get("password", None)
+#
+#
+#     access_token = create_access_token(identity=username)
+#     return jsonify(access_token=access_token)
+#
+#
+# @app.route("/protected", methods=["GET"])
+# @jwt_required()
+# def protected():
+#     # Access the identity of the current user with get_jwt_identity
+#     current_user = get_jwt_identity()
+#     return jsonify(logged_in_as=current_user), 200
+#
+#
+# @app.route('/api/hello-world')
+# def hello_world():
+#     from db.models import User
+#     user = User.query.filter_by(login='admin').first()
+#     return str(user.id)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 
+
+from api.v1.auth import auth_bp
 from core import config
 from db.pg_db import init_db, db
 
@@ -15,6 +17,10 @@ jwt = JWTManager(app)
 
 init_db(app=app)
 
+API_V1_PATH = '/api/v1'
+app.register_blueprint(auth_bp, url_prefix=API_V1_PATH + '/auth')
+
+
 with app.app_context():
     from db import models
     db.create_all()
@@ -23,6 +29,9 @@ with app.app_context():
 @app.route('/api/hello-world')
 def hello_world():
     from db.models import User
+    admin = User(login='admin1', password=hash_password('password'))
+    db.session.add(admin)
+    db.session.commit()
     user = User.query.filter_by(login='admin').first()
     return str(user.id)
 
