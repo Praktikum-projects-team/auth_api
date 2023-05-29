8413d12daa887743b37315eda035392dc80533bf
tavrora <46925576+tavrora@users.noreply.github.com>
2023-05-29 15:09:36 +0300

    Feat/api admin users (#21)
    * add a method user info
    
    * assign a user a role on registration
    
    * add a method all users info
    
    * add a method update user info
    
    * edits after review
    
    * quote refactoring
    
    * add postman_collection for api
    
    * edits after review 2
    
    ---------
    
    Co-authored-by: Tatyana Akimova <takimova@ivi.ru>

--- src/db/queries/user.py
+++ src/db/queries/user.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 
-from db.models import User, LoginHistory
+from constants import RoleName
+from db.models import Role, User, LoginHistory
 from db.pg_db import db
 
 
@@ -13,7 +14,15 @@ def does_user_exist(login: str):
 
 
 def create_new_user(user):
-    new_user = User(**user)
+    # Создаем роль, если она не существует
+    role = db.session.query(Role).filter(Role.name == RoleName.USER).first()
+    if not role:
+        new_role = Role(name=RoleName.USER)
+        db.session.add(new_role)
+        db.session.commit()
+
+    # Вносим пользователя в базу
+    new_user = User(**user, roles=[role])
     db.session.add(new_user)
     db.session.commit()
 
