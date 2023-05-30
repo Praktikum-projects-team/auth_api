a73cf1ee578ee682bbf06aed433a8591ed335dfd
liana <liana.nigmatullina@tetrika.school>
2023-05-30 13:23:08 +0300

    fix/add_superuser_to_role_checker

--- src/services/auth/role_checker.py
+++ src/services/auth/role_checker.py
@@ -8,16 +8,16 @@ from constants import RoleName
 from db.queries.user import get_user_by_login
 
 
-def role_required(role):
+def role_required(required_role):
     def role_decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             verify_jwt_in_request()
             user_login = get_jwt_identity()
             user = get_user_by_login(user_login)
-            role_names = [r.name for r in user.roles]
-            if role not in role_names:
-                return jsonify(message=f'user must have role {role}'), HTTPStatus.FORBIDDEN
+            user_roles = [r.name for r in user.roles]
+            if not user.is_superuser and required_role not in user_roles:
+                return jsonify(message=f'user must have role {required_role}'), HTTPStatus.FORBIDDEN
             result = func(*args, **kwargs)
             return result
         return wrapper
