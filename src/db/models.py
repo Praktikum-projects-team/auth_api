b61b5547bf854e311518a785058b9bed74fa2934
Sofia <77271885+rsof14@users.noreply.github.com>
2023-06-17 11:51:53 +0300

    Fix/oauth (#43)
    * Oauth service
    
    * Deleted unnecessary code
    
    * MVC changes
    
    * Deleted unnecessary libs
    
    * Model and migration for oauth account table
    
    * Changed oauth table

--- src/db/models.py
+++ src/db/models.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import Boolean, Column, DateTime, ForeignKey, String
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, String, Text
 from sqlalchemy.dialects.postgresql import UUID
 
 from db.pg_db import db
@@ -69,3 +69,11 @@ class UserRole(db.Model):
     user_id = Column(UUID(as_uuid=True), ForeignKey(User.id), primary_key=True, nullable=False)
     role_id = Column(UUID(as_uuid=True), ForeignKey(Role.id, ondelete='CASCADE'), primary_key=True, nullable=False)
     given_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+
+
+class OauthAccounts(db.Model):
+    __tablename__ = 'oauth_accounts'
+
+    user_id = Column(UUID(as_uuid=True), ForeignKey(User.id), primary_key=True, nullable=False)
+    oauth_user_login = Column(Text, nullable=False)
+    provider = Column(String(100), primary_key=True, nullable=False)
