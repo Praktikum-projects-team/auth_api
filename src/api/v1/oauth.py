0d9e68c1ed00551efe0ee286d66c18c2c8017b2b
liana <liana.nigmatullina@tetrika.school>
2023-06-17 15:29:39 +0500

    use oauth create_client

--- src/api/v1/oauth.py
+++ src/api/v1/oauth.py
@@ -11,15 +11,17 @@ oauth_bp = Blueprint("oauth_bp", __name__)
 @oauth_bp.route('/<provider>/authorize')
 def login(provider: str):
     redirect_uri = url_for('oauth_bp.authorize', _external=True, provider=provider)
-    return oauth.google.authorize_redirect(redirect_uri)
+    provider_client = oauth.create_client(provider)
+    return provider_client.authorize_redirect(redirect_uri)
 
 
 @oauth_bp.route('/<provider>/oauth2callback')
 def authorize(provider: str):
-    token = oauth.google.authorize_access_token()
+    provider_client = oauth.create_client(provider)
+    token = provider_client.authorize_access_token()
     # resp = oauth.google.get('userinfo', token=token)
     # user_info = resp.json()
-    userinfo = oauth.google.userinfo()
+    userinfo = provider_client.userinfo()
 
     oauth_user = get_oauth_user(userinfo, provider)
     tokens = login_by_oauth(oauth_user, provider)
