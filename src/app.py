1c9cf385f2ae5835aeb209c21fee00c1759f2345
tolya-habarov <41547021+tolya-habarov@users.noreply.github.com>
2023-07-24 19:17:29 +0700

    add sentry sdk (#48)
    * add sentry sdk
    
    * fix sentry version
    
    * early sentry initialization
    
    ---------
    
    Co-authored-by: Tolya Khabarov <a.khabarov@cdek.ru>

--- src/app.py
+++ src/app.py
@@ -1,11 +1,13 @@
 import logging
 
 import logstash
+import sentry_sdk
 from flask import Flask, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_migrate import upgrade
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 from api.v1.admin_roles import admin_roles_bp
 from api.v1.admin_users import admin_users_bp
@@ -13,12 +15,19 @@ from api.v1.auth import auth_bp
 from api.v1.models.marshmallow_init import init_marshmallow
 from api.v1.oauth import oauth_bp
 from api.v1.users import users_bp
-from core.config import app_config
+from core.config import app_config, sentry_config
 from core.oauth_init import init_oauth
 from core.tracing import configure_tracer
 from db.alembic_migrate_init import init_migration_tool
 from db.pg_db import db, init_db
 from services.auth.jwt_init import init_jwt
+
+
+sentry_sdk.init(
+    dsn=sentry_config.dns,
+    integrations=[FlaskIntegration()],
+    traces_sample_rate=0.4
+)
 
 
 def register_blueprints(app):
